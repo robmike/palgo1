1afac86be79f97e5c5c973ec259baced3e862854
invalid <invalid@invalid>
2013-02-17 17:37:04 -0500

    more test cases

--- hw2/test.java
+++ hw2/test.java
@@ -6,11 +6,17 @@ public class test {
     public static void main(String[] args) throws IOException {
         Deque<Integer> d = new Deque<Integer>();
         for(int i=0; i<10; i++){
-            d.addFirst(i);
-            System.out.println(d.nitems);
+            d.addLast(i);
+            System.out.println(i);
         }
-        for(int i=0; i<10; i++){
+        for(int i=0; i<5; i++){
             System.out.println(d.removeLast());
+        }
+        for(int i=100; i<105; i++){
+            d.addLast(i);
+        }
+        for(Integer i : d){
+            System.out.println(i);
         }
     }
 }
